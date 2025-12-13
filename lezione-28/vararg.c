1f71ddf384edf7688dab3fc34517b636d75cd439
Andrea Coi <andrea.coi@outlook.it>
2025-12-13 17:01:28 +0100

    completa lezione 28 sulle funzioni variadiche

--- lezione-28/vararg.c
+++ lezione-28/vararg.c
@@ -7,21 +7,43 @@
  * Poi però questi (il loro numero e la lunghezza di ciascuno di essi) vanno
  * gestiti con va_list, va_start, va_arg e per chiudere va_end.
  * Queste sono delle MACRO e sono contenute nella libreria stdarg.h.
+ * UTILIZZO:
+ * va_start DEVE AVERE come argomenti:
+ *  - una variabile di tipo va_list (ap);
+ *  - l'ultimo argomento della funzione NON variabile (char *fmt)
+ * in questo modo va_start sa che deve iniziare a elaborare gli argomenti
+ * variabili dopo char *fmt.
  */
-void foo(char *bar, ...) {
+void foo(char *fmt, ...) {
+  // dichiaro il mio oggetto ap, che conterrà gli oggetti da estrarre
   va_list ap;
-  // va_start DEVE AVERE come argomenti:
-  // - una variabile di tipo va_list (ap);
-  // - l'ultimo argomento della funzione NON variabile (char *bar)
-  // in questo modo va_start sa che deve iniziare a elaborare gli argomenti
-  // variabili dopo char *bar.
-  va_start(ap, bar);
-  printf("%s\n", bar);
+  va_start(ap, fmt);
+  char *p = fmt;
+  while (*p) {
+    switch (*p) {
+      case 'i':
+        printf("%d\n", va_arg(ap, int));
+        break;
+      case 's':
+        printf("%s\n", va_arg(ap, char*));
+        break;
+      default:
+        printf("Wrong specifier.\n");
+        // in questo specifico caso conviene usare goto, evitiamo il return
+        // da così: 
+        // va_end(ap);
+        // return;
+        // diventa:
+        goto cleanup;
+    }
+    p++;
+  }
+cleanup:
+  va_end(ap);
 }
 
 int main(void) {
   printf("Hello World\n");
-  char *bar = "gino";
-  foo(bar);
+  foo("isii", 5, "ciao ciao", 10, 20);
   return 0;
 }
